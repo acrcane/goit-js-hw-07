cea388ea7e3d915a2f210ad14165f6696fe83fba
arcane <arcanesir.neo@gmail.com>
2023-03-27 22:02:03 +0200

    full

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,47 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const lightboxContainer = document.querySelector('.gallery');
+
+addToHtml(lightboxContainer)
+
+lightboxContainer.addEventListener('click', clickImages)
+
+function createLightboxGallery(array){
+    return array.map(({preview, original, description}) => {
+        return `<li class="gallery__item" >
+                    <a href="${original}" class="gallery__link">
+                        <img src="${preview}" alt="${description}" data-source="${original}" class="gallery__image">
+                    </a>
+                </li>`           
+    }).join('')
+}
+console.log(createLightboxGallery(galleryItems));
+
+function addToHtml(container){
+    container.innerHTML = createLightboxGallery(galleryItems)
+}
+
+function clickImages(event){
+    event.preventDefault()
+    if(event.target.nodeName !== "IMG"){
+        return
+    }
+    const instance = basicLightbox.create(`<img src="${event.target.dataset.source}" width="800" height="600">`,
+        {onShow: () => {
+            document.addEventListener('keydown', closeWindowEsc)
+        },
+        onClose: () => {
+            document.removeEventListener('keydown', closeWindowEsc)
+        }
+    }
+    )
+    instance.show()
+
+    function closeWindowEsc(event){
+        if(event.code !== 'Escape'){
+            return
+        }
+        instance.close()
+    }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const lightboxContainer = document.querySelector('.gallery');
+
+addToHtml(lightboxContainer)
+
+lightboxContainer.addEventListener('click', showImage)
+
+function createLightboxGallery(array){
+    return array.map(({preview, original, description}) => {
+        return `<li class="gallery__item" >
+                    <a href="${original}" class="gallery__link">
+                        <img src="${preview}" alt="${description}" data-source="${original}" class="gallery__image">
+                    </a>
+                </li>`           
+    }).join('')
+}
+console.log(createLightboxGallery(galleryItems));
+
+function addToHtml(container){
+    container.innerHTML = createLightboxGallery(galleryItems)
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250
+})
+
+function showImage(){
+    lightbox.on('show.simplelightbox')
+}
